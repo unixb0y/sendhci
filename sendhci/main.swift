3764af56bb9fc7e3318d75f90f526ff793577517
Davide Toldo <davide.toldo@stud.tu-darmstadt.de>
2020-04-22 18:09:27 +0200

    Initial Commit

---
+++ sendhci/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  sendhci
+//
+//  Created by Davide Toldo on 22.04.20.
+//  Copyright © 2020 Davide Toldo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
